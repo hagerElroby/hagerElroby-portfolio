5036ce162460408cf67105e17cdc876d8ddd5b03
hagerElroby <hagerelroby.az2020@gmail.com>
2023-08-16 20:21:57 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/data.js
@@ -0,0 +1,36 @@
+
+
+export const products = [
+      {
+        id: 1,
+        img: require('./img/2.jpg'),
+        link: "https://hagerelroby.github.io/nokoSocial-/",
+      },
+      {
+        id: 2,
+        img: require('./img/1.png'),
+        link: "https://clone-yout.web.app/",
+      },
+      {
+        id: 3,
+        img: require('./img/6.png'),
+        link: "https://ecommerce-5kq2.onrender.com/",
+      },
+      {
+        id: 4,
+        img:  require('./img/4.jpg'),
+        link: "https://react18-gym-responsive-app.vercel.app/",
+      },
+      {
+        id: 5,
+        img: require('./img/fd.jpg'),
+        link: "https://hagerelroby.github.io/Agency_Template-/",
+      },
+      {
+        id: 6,
+        img: require('./img/gg.png'),
+        link: "https://hagerelroby.github.io/Native_ECommerce_js/",
+      },
+    ];
+    
+    
